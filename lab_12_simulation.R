15786d5e18009c0b6c7358786ed16bab08102b74
unknown <kouyang@andrew.cmu.edu>
2018-04-19 08:26:23 -0600

    correct data generatefunction

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,5 +3,5 @@ generate_data=function(n, p){
   for (i in 1:length(response)){
     response[i]=rnorm(1)
   }
-  return(covarites=matrix(rnorm(n*p),nrow=n,ncol = p),responses=vect)
+  return(list(covarites=matrix(rnorm(n*p),nrow=n,ncol = p),responses=response))
 }
