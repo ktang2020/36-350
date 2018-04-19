3400e0ea04273c6e14e0df2edf9710d12f4835b5
ktang2020 <38506107+ktang2020@users.noreply.github.com>
2018-04-19 06:39:11 -0600

    Delete lab_12_simulation.R

--- lab_12_simulation.R
+++
@@ -1,7 +0,0 @@
-generate_data=function(n, p){
-  response=vector(length=n)
-  for (i in 1:length(response)){
-    response[i]=rnorm(1)
-  }
-  return(list(covarites=matrix(rnorm(n*p),nrow=n,ncol = p),responses=response))
-}
