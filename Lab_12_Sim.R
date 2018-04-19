a6c72edb0f861c59a28fda834f6d6b2d80dbe760
ktang2020 <38506107+ktang2020@users.noreply.github.com>
2018-04-19 07:37:37 -0600

    Update Lab_12_Sim.R

--- Lab_12_Sim.R
+++ Lab_12_Sim.R
@@ -1,7 +1,7 @@
 generate_data=function(n, p){
   responses=vector(length=n)
-  for (i in 1:length(response)){
+  for (i in 1:length(responses)){
     responses[i]=rnorm(1)
   }
-  return(list(covarites=matrix(rnorm(n*p),nrow=n,ncol = p),responses=responses))
+  return(list(covariates=matrix(rnorm(n*p),nrow=n,ncol = p),responses=responses))
 }
